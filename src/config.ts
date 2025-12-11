c3985dc554c5a05d59156970e07cbe5d20443c80
Valmo Trindade <val_mo_jr@hotmail.com>
2025-12-10 21:37:29 -0300

    matcehd config.ts as type valitor for env

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_ADDRESS: string = "192.168.15.10";
-export const SERVER_PORT: number = 4242;
-export const GROUP_NUMBER: number = 8;
-export const SOCKET_TYPE: "TCP" | "UDP" = "UDP";
+export const SERVER_ADDRESS: string = process.env.SERVER_ADDRESS || "192.168.15.10";
+export const SERVER_PORT: number = Number(process.env.SERVER_PORT) || 4242;
+export const GROUP_NUMBER: number = Number(process.env.GROUP_NUMBER) || 8;
+export const SOCKET_TYPE: "TCP" | "UDP" = (process.env.SOCKET_TYPE === "TCP" ? "TCP" : "UDP");
